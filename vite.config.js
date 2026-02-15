d689a59194062c4fa1571038c56e20555c2ea9f1
zoeplatform <marcojyysantos@gmail.com>
2026-02-14 23:59:37 -0300

    Change base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-
 export default defineConfig({
-plugins: [react()],
-base: "/zoefinan/",
+  plugins: [react()],
+  base: "/", // ✅ para zoefinan.com
 });
